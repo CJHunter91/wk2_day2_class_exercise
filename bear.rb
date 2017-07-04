749789181687489ee02cba2873b078dc35308255
Chris Hunter <orkem2@gmail.com>
2017-07-04 11:54:01 +0100

    Added bear_has_name test passed

--- bear.rb
+++ bear.rb
@@ -1,2 +1,10 @@
 require_relative('./fish.rb')
 require_relative('./river.rb')
+
+
+class Bear
+  attr_reader :name
+    def initialize(name)
+      @name = name
+    end
+end 
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -4,3 +4,9 @@ require_relative('../river.rb')
 require_relative('../bear.rb')
 
 
+class TestBear < MiniTest::Test
+  def test_bear_has_name()
+    bear = Bear.new("Yogi")
+    assert_equal("Yogi", bear.name())
+  end
+end
