bd6a0c99e8623567b1727532c3bcf7ca793e76c2
Chris Hunter <orkem2@gmail.com>
2017-07-04 11:56:09 +0100

    Added river class and test river has name passed

--- river.rb
+++ river.rb
@@ -1,2 +1,9 @@
 require_relative('./bear.rb')
 require_relative('./fish.rb')
+
+class River
+  attr_reader :name
+    def initialize(name)
+      @name = name
+    end
+end 
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -2,3 +2,10 @@ require('minitest/autorun')
 require_relative('../fish.rb')
 require_relative('../river.rb')
 require_relative('../bear.rb')
+
+class TestRiver < MiniTest::Test
+  def test_river_has_name()
+    river = River.new("Amazon")
+    assert_equal("Amazon", river.name())
+  end
+end
